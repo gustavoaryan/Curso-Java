7b8d68602d98d901cc5f24efef7442546d60a29d
gustavoaryan <gustavoaryan@gmail.com>
2025-04-03 21:32:08 -0300

    second-commit

--- src/main/java/exercicios/Exercicio2.java
+++ src/main/java/exercicios/Exercicio2.java
@@ -1,5 +1,7 @@
 package exercicios;
 
+import java.util.Locale;
+
 public class Exercicio2 {
     public static void main(String[] args){
         String product1 = "Computer";
@@ -13,6 +15,16 @@ public class Exercicio2 {
         double price2 = 650.50;
         double measure = 53.234567;
 
+        System.out.println("Products: ");
+        System.out.printf("%s, which price is $ %.2f%n", product1, price1);
+        System.out.printf("%s, which price is $ %.2f%n%n", product2, price2);
+
+        System.out.printf("Record: %d years old, code %d and gender: %c%n%n", age, code, gender);
+
+        System.out.printf("Measue with eight decimal places: %.8f%n", measure);
+        System.out.printf("Rouded (three decimal places: %.5f%n", measure);
+        Locale.setDefault(Locale.US);
+        System.out.printf("US decimal point: %.3f%n", measure);
 
     }
 }
